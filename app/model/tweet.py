2174f3c2e12d9f25fe9a397ee33a41adca2d23a2
fahmisulthoni <fahmisulthoni30@gmail.com>
2021-09-26 11:54:38 +0700

    initial deploy

--- app/model/tweet.py
+++ app/model/tweet.py
@@ -5,7 +5,7 @@ class Tweets(db.Model) :
     id = db.Column(db.BigInteger, primary_key=True, autoincrement=True)
     tweet = db.Column(db.String(280), nullable=False)
     url = db.Column(db.String(200), index=True, unique=True,nullable=False)
-    result = db.Column(db.String(4), nullable=False)
+    result = db.Column(db.String(10), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self) :
--- config.py
+++ config.py
@@ -11,6 +11,6 @@ class Config(object) :
     #PASSWORD = "amisijenius303"
 
     #SQLALCHEMY_DATABASE_URI = 'postgresql://' + USERNAME + ':' + PASSWORD + '@' + HOST + '/' + DATABASE
-    SQLALCHEMY_DATABASE_URI = 'postgresql://rbhrlraspwsegz:22e02ef18e5585c8bf7cb19b8e44f5303e10e6c68cb3b6bfb483eff97c5d8b6a@ec2-34-233-187-36.compute-1.amazonaws.com:5432/d7a92tefva6ubq'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://dpklknarnehyzd:d04c92f1c6356b83edfd56abcf6526be787b7b4dbd44160a82aa3a1b60a57078@ec2-54-145-110-118.compute-1.amazonaws.com:5432/dcjhounp5d7bec'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_RECORD_QUERIES = True

58de945c23e29f4be8f66f7c50331a95ee139764
fahmisulthoni <fahmisulthoni30@gmail.com>
2021-09-27 08:54:56 +0700

    initial deploy

--- config.py
+++ config.py
@@ -11,6 +11,6 @@ class Config(object) :
     #PASSWORD = "amisijenius303"
 
     #SQLALCHEMY_DATABASE_URI = 'postgresql://' + USERNAME + ':' + PASSWORD + '@' + HOST + '/' + DATABASE
-    SQLALCHEMY_DATABASE_URI = 'postgresql://dpklknarnehyzd:d04c92f1c6356b83edfd56abcf6526be787b7b4dbd44160a82aa3a1b60a57078@ec2-54-145-110-118.compute-1.amazonaws.com:5432/dcjhounp5d7bec'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://usjlxebtamfiwz:c1d95733083830acc28e052ab29355bd14d78614bfef9444576242862aeefd13@ec2-18-209-143-227.compute-1.amazonaws.com:5432/dd0eeb7v99m4en'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_RECORD_QUERIES = True
